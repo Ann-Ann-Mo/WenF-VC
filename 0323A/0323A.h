1460436ac8871f9e861c9e6601ba8ca74b227770
Ann-Ann-Mo <1536062597@qq.com>
2020-04-13 21:02:58 +0800

    123提交

---
+++ 0323A/0323A.h
@@ -0,0 +1,33 @@
+
+// 0323A.h : 0323A 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CMy0323AApp:
+// 有关此类的实现，请参阅 0323A.cpp
+//
+
+class CMy0323AApp : public CWinAppEx
+{
+public:
+	CMy0323AApp();
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMy0323AApp theApp;
